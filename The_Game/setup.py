e2ce8e347ece5d4769bd8677f55e93194f6a763e
RishabhSood <55499929+RishabhSood@users.noreply.github.com>
2020-01-14 22:01:45 +0530

    updated as per requirements

--- The_Game/setup.py
+++ The_Game/setup.py
@@ -3,9 +3,9 @@ import cx_Freeze
 executables = [cx_Freeze.Executable("game.py")]
 
 cx_Freeze.setup(
-    name="A bit Racey",
+    name="WorldWarIII",
     options={"build_exe": {"packages":["pygame"],
-                           "include_files":["trump.png", "WW3music.mp3", "warback.jpg"]}},
+                           "include_files":["trump.png", "WW3music.mp3", "warback.jpg", "uswins.mp3", "function_lib.py"]}},
     executables = executables
 
     )
