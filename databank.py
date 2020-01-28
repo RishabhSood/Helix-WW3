37d4e0fcff3c194bf1d6bb10da14882b9cda6c33
RishabhSood <55499929+RishabhSood@users.noreply.github.com>
2020-01-28 20:15:52 +0530

    Add files via upload

---
+++ databank.py
@@ -0,0 +1,84 @@
+
+Question Database
+
+1> Path: Iran
+
+Q1. Title: Somewhere in Iran
+	Content:  1,150,000 people stand in a circle. One of them is an undercover agent. There is a sword in 
+              the hand of the 1st person, he kills the 2nd person and passes on the sword to the third, 
+              who kills the fourth and gives the sword to the 5th, this goes on until one person is left. 
+              At what position does the spy stand to survive?
+    Ans: 202849
+
+Q2. Function: iran()
+	Title: Held Hostage..
+	Content:  Some of your soldiers have been held hostage by Iran Military. To set them free, you need access to their database. The database is distributed amongst 21 servers. It is known that each server has the same password. However, you've already attempted access to 20/21 servers and been blocked. This is your last chance! Following information has been gathered from the previous attacks:
+
+
+	Password     CC
+	228569150065 1
+	907564288621 0
+	496954400043 0
+	713459943615 0
+	211421327491 1
+	258317293172 0
+	919252724339 1
+	197103476352 0
+	151173430038 0
+	063794395936 0
+	504759866532 1
+	502906565456 0
+	790539816536 0
+	595873942664 1
+	346602334981 0
+	988808475766 1
+	559203789779 0
+	498580144863 1
+	441454897857 1
+	622818801178 0
+
+	Here, CC is number of correct digits at correct place in the password.
+	Enter password to the server below:
+
+	Ans: 884045122207
+
+Q3. Function: iran_solo_inv()
+	Title: Plane Hijack
+	Content: You have decided to enter Iran in stealth mode. Crack password to their base. Following information about the current code has been leaked by an insider. See if you can crack it:
+	The password is last 5 digits of the sum of all integers n, 10<n<10^17, having property as depicted:
+
+	Consider the number 142857. We can right-rotate this number by moving the last digit 7 to the front of it, giving us 714285.
+	It can be verified that 714285 = 5 * 142857. This demonstrates an unusual property of 142857: it is a divisor of its right-rotation.
+
+	Ans: 75309
+
+Q4. Function: iran_solo_redwed()
+    Title: Convince The Supplier
+    Content: The supplier doesn't trust anyone without a code!! You are new and have been entrusted with the task of arranging ammunition. Enter the code below. Follow the gudelined below to get the code:
+
+    Consider the triangle below:
+     1
+    |2| |3|
+     4  |5|  6
+    |7 | 8   9   10
+    |11| 12 |13| 14 15
+    ...
+
+    A set of three primes is called a prime triplet if one of the three primes has the other two as neighbours in the triangle. (Neighbors refers to top, below, left, right and diagonal ones, MAX 8).
+    (|n| is a prime triplet member)
+    We define S(n) as the sum of the primes in row n which are elements of any prime triplet.
+    For example, S(5) = 24 and S(2) = 5.
+
+    Key = S(10000)
+
+    Ans: 950007619
+
+Q5. Function: iran_red_succ()
+    Title: Formation
+    Content: Fighting off their deffences will need a certain formation for the army. This formation has been scribbled on a document present inside a safe. Access the document and fight off their deffences!!. Enter the safe code below.
+
+    Safe Code: Sum of all n, 0 <= n <= 100, such that f(n) is a perfect square modulo 10^9 + 7.
+    Here, f(n) is the sum of the squares of the digits (in base 10) of n, ex:
+    f(442) = 4^2 + 4^2 + 2^2 = 36
+
+    Ans: 826
