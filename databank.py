b06884ca84d435bc7b55e8315a501623c1b3a127
RishabhSood <55499929+RishabhSood@users.noreply.github.com>
2020-02-01 00:49:23 +0530

    Update databank.py

--- databank.py
+++ databank.py
@@ -4,6 +4,7 @@ Question Database
 1> Path: Iran
 
 Q1. Title: Somewhere in Iran
+	Points: 50
 	Content:  1,150,000 people stand in a circle. One of them is an undercover agent. There is a sword in 
               the hand of the 1st person, he kills the 2nd person and passes on the sword to the third, 
               who kills the fourth and gives the sword to the 5th, this goes on until one person is left. 
@@ -11,6 +12,7 @@ Q1. Title: Somewhere in Iran
     Ans: 202849
 
 Q2. Function: iran()
+	Points: 300
 	Title: Held Hostage..
 	Content:  Some of your soldiers have been held hostage by Iran Military. To set them free, you need access to their database. The database is distributed amongst 21 servers. It is known that each server has the same password. However, you've already attempted access to 20/21 servers and been blocked. This is your last chance! Following information has been gathered from the previous attacks:
 
@@ -43,6 +45,7 @@ Q2. Function: iran()
 	Ans: 884045122207
 
 Q3. Function: iran_solo_inv()
+	Points: 300
 	Title: Plane Hijack
 	Content: You have decided to enter Iran in stealth mode. Crack password to their base. Following information about the current code has been leaked by an insider. See if you can crack it:
 	The password is last 5 digits of the sum of all integers n, 10<n<10^17, having property as depicted:
@@ -53,6 +56,7 @@ Q3. Function: iran_solo_inv()
 	Ans: 75309
 
 Q4. Function: iran_solo_redwed()
+	Points: 200
     Title: Convince The Supplier
     Content: The supplier doesn't trust anyone without a code!! You are new and have been entrusted with the task of arranging ammunition. Enter the code below. Follow the gudelined below to get the code:
 
@@ -74,6 +78,7 @@ Q4. Function: iran_solo_redwed()
     Ans: 950007619
 
 Q5. Function: iran_red_succ()
+	Points: 150
     Title: Formation
     Content: Fighting off their deffences will need a certain formation for the army. This formation has been scribbled on a document present inside a safe. Access the document and fight off their deffences!!. Enter the safe code below.
 
@@ -82,3 +87,47 @@ Q5. Function: iran_red_succ()
     f(442) = 4^2 + 4^2 + 2^2 = 36
 
     Ans: 826
+
+Q6. Function: iran_stealth()
+	Points: 50
+	Title: Hide the files!!
+	Content: Out of the 1000 files of evidence, one proves your involvement. However, the labor
+            available at hand is unskilled and incapable of reading. The evidence file has a special 
+            property, it leave an impression on the hands of whoever touched it, which turns blue within
+            10 to 20 hours of contact. The impression is however, otherwise undetectable. What is the 
+            minimum amount of labor you will require to find the file within 24hrs ?
+	Ans: 10
+
+Q7. Function: 
+	Points: 800
+	Title: Secure their Network
+	Content: Make them accept your explanation, by helping them with the Following problem:
+	To cut costs, UN wished to power their network with their own special encryption. Each server would further 
+	encrypt and send information to the server numbered next, with the top server sending data back down 
+    to the bottom server. That way, they could have the information entered on any node and have data flow 
+    freely throughout the network.
+
+    Unfortunately, the designers misinterpreted the schematics when they built the network. It was leaked that 
+    each server sends data to another server at random, instead. 
+    This may compromise their freedom to have the data entering the network anywhere, since certain nodes may 
+    never recieve the info and hence increase data loss risks.
+
+    For example, consider the following connections in a 3 server based network with servers numbered 1,2 and 3 
+    in order:
+    1 connects to 3
+    3 connects to 2
+    and 2 connects to 3
+
+    If the information was fed in the first node, then every server would receive data. But if it were fed in the 
+    second or third servers instead, then there would be no data entering first server. Note that while a given 
+    server can receive data from many other servers at once, it can only send data to one other server.
+
+    To resolve the data loss risks, UN decided to have a minimal number of servers rewired. To rewire a server is 
+    to change the server it sends data to. In the sample above, all possible losses can be avoided by rewiring 
+    the second server to send data to the first server instead of the third server.
+
+    Let F(n) be the sum of the minimum number of server rewirings needed over all possible data-flow arrangements
+    in a network of n servers. For example, F(3) mod 135707531 = 6.
+    Find F(8) mod 135707531.
+
+	Ans: 16276736
