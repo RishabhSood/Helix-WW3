43486e1a45bf44482090e3913ac4fbf2fc5a4696
RishabhSood <55499929+RishabhSood@users.noreply.github.com>
2020-02-02 01:01:51 +0530

    added score and log file

--- The_Game/setup.py
+++ The_Game/setup.py
@@ -3,9 +3,9 @@ import cx_Freeze
 executables = [cx_Freeze.Executable("game.py")]
 
 cx_Freeze.setup(
-    name="WorldWarIII",
+    name="World War III",
     options={"build_exe": {"packages":["pygame"],
-                           "include_files":["trump.png", "WW3music.mp3", "warback.jpg", "uswins.mp3", "function_lib.py"]}},
+                           "include_files":["trump.png", "WW3music.mp3", "warback.jpg", "uswins.mp3", "function_lib.py", "score.txt", "ww3_log.txt"]}},
     executables = executables
 
     )
