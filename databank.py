912792239d0b71de302874ac764411c084fe3fc4
AulonSal <AulonSal@gmail.com>
2020-02-04 05:00:41 +0530

    Completed the databank, still need someone to test, though I do have all the question souces written in anther file in case I screwed up somewhere.

--- databank.py
+++ databank.py
@@ -216,9 +216,9 @@ Q14. Func: Ashen Victory
 	4213 + 3124 = 7337
 
 	That is, 349 took three iterations to arrive at a palindrome.
-	It has long been thought by sages that some numbers never arrive at a palindrome, 50 is a big number, numbers that do not fall in line(become a palindrome) in 50
+	It has long been thought by sages that some numbers never arrive at a palindrome, since 50 is a big number, numbers that do not fall in line(become a palindrome) in 50
 	iterations are to be deemed Budget-Lychrel numbers and are the secret to victory.
-	How many Lychrel Numbers are there below ten-thousand.
+	How many Budget-Lychrel Numbers are there below ten-thousand.
 
 	Ans: 249
 
@@ -244,9 +244,264 @@ Q15. Func: Impeach
 	Ans: 26241
 
 
+Q35. Func: 
+	Points: 150
+	Title: Is all fine! No need to Panic!!
+	Content:You try to convince the room that everything is under control.
+	To prepare yourself for logical arguments you decide to practice with some quick maths:
+	A googol (10100) is a massive number: one followed by one-hundred zeros; 100100 is almost unimaginably large: one followed by two-hundred zeros. Despite their size, the sum of the digits in each number is only 1.
+	Considering natural numbers of the form, ab, where a, b < 100, what is the maximum digital sum?
+	
+	Ans: 972
+
+Q37. Func: 
+	Points: 150
+	Title: Dratted Computers.
+	Content:To convince this computer with more brains than the rest of the gathering combined you must fool it with loops in logic that it cannot grasp.
+	The number 145 is well known for the property that the sum of the factorial of its digits is equal to 145:
+
+	1! + 4! + 5! = 1 + 24 + 120 = 145
+
+	Perhaps less well known is 169, in that it produces the longest chain of numbers that link back to 169; it turns out that there are only three such loops that exist:
+
+	169 → 363601 → 1454 → 169
+	871 → 45361 → 871
+	872 → 45362 → 872
+
+	It is not difficult to prove that EVERY starting number will eventually get stuck in a loop. For example,
+
+	69 → 363600 → 1454 → 169 → 363601 (→ 1454)
+	78 → 45360 → 871 → 45361 (→ 871)
+	540 → 145 (→ 145)
+
+	Starting with 69 produces a chain of five non-repeating terms, but the longest non-repeating chain with a starting number below one million is sixty terms.
+
+	How many chains, with a starting number below one million, contain exactly sixty non-repeating terms?
+
+	Ans: 402
+
+Q40. Func: 
+	Points: 100
+	Title: We must declare war... Yesterday!
+	Content:You try to convince the gathering that the best course of action is to start this battle more awaited than Cleganebowl.
+	War is war no matter how you phrase it, but selectively forming your arguments might help with some of the more pacifist types here.
+	
+	There are exactly ten ways of selecting three from five, 12345:
+	123, 124, 125, 134, 135, 145, 234, 235, 245, and 345
+	In we, use combinatorics the notation, 5C3=10
+
+	In general, (nCr)=n! * r! / (n−r)!, where r≤n, n!=n×(n−1)×...×3×2×1, and 0!=1
+	It is not until n=23, that a value exceeds one-million: (2310)=1144066
+	
+	How many, not necessarily distinct, values of (nr)
+	for 1≤n≤100, are greater than one-million?
+	
+	Ans: 4075
 
 
+Q43. Func: 
+	Points: 140
+	Title: Numbers War
+	Content: The Russian digital network works on the basis of powers of 2, to attack it, you must use higher powers, as bigger is better.
+	
+	The 5-digit number, 16807=75, is also a fifth power. Similarly, the 9-digit number, 134217728=89, is a ninth power.
+	How many n-digit positive integers exist which are also an nth power?
+	
+	Ans: 49 
+
+Q45. Func: 
+	Points: 500
+	Title: Our Glorious Allies!
+	Content: You try to Convince Nato of your war being Just!
+	You plan to go about this using a roundabout analogy about how what you're doing truly is the most sensible thing to do.
+	
+	Five pirates have obtained 100 gold coins and have to divide up the loot. The pirates are all extremely intelligent, treacherous and selfish (especially the captain).
+
+	The captain always proposes a distribution of the loot. All pirates vote on the proposal, and if half the crew or more go "Aye", the loot is divided as proposed, as no pirate would be willing to take on the captain without superior force on their side.
+
+	If the captain fails to obtain support of at least half his crew (which includes himself), he faces a mutiny, and all pirates will turn against him and make him walk the plank. The pirates start over again with the next senior pirate as captain.
+
+	What is the maximum number of coins the captain can keep without risking his life?
+	
+	Ans: 98
+
+Q48. Func: 
+	Points: 120
+	Title: War is On!
+	Content:You must deal with logistics issues, you have supplies for a million troops for protracted war but you need to support 2 million.
+	
+	Let a0, a1, a2, ... be an integer sequence defined by:
+
+		a0 = 1;
+		for n ≥ 1, an is the sum of the digits of all preceding terms.
+
+	The sequence starts with 1, 1, 2, 4, 8, 16, 23, 28, 38, 49, ...
+	You are given a10^6 = 31054319.
+
+	Find a(2 x 10^6)
+	
+	Ans: 65514710
+
+Q50. Func: 
+	Points: 110
+	Title: Set it and Forget It
+	Content:You do not even turn to look as the White House, the symbol of the Presidency, the United States government,
+	and the American people, explodes in flames as you board your ex-Presidential Jet to Moscow.
+	
+	Now, you must dodge the jets that'll soon bar your way, you must become one with the wind, and with the planes that once would've protected you, mirroring them but passing through.
+	A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+	Find the largest palindrome made from the product of two 3-digit numbers.
+
+	Ans: 906609
 
 
+Q52. Func: 
+	Points: 110
+	Title: Pussy Riot
+	Content: Prime your weapons for the final charge on the Kremlin.
+	
+	The first known prime found to exceed one million digits was discovered in 1999, and is a Mersenne prime of the form 26972593−1; it contains exactly 2,098,960 digits. Subsequently other Mersenne primes, of the form 2p−1, have been found which contain more digits.
 
+	However, in 2004 there was found a massive non-Mersenne prime which contains 2,357,207 digits: 28433×27830457+1.
+
+	Find the last ten digits of this prime number.
+
+	Ans: 8739992577
+
+Q57. Func: 
+	Points:150
+	Title: Most Powerful man for Sale
+	Content: You try to sell your importance to the Kremlin, chaining yourself to the Russian Cause, 
+	with handwritten notes detailing chains of supply and what not that'll spell america's doom.
+
+	A number chain is created by continuously adding the square of the digits in a number to form a new number until it has been seen before.
+
+	For example,
+
+	44 → 32 → 13 → 10 → 1 → 1
+	85 → 89 → 145 → 42 → 20 → 4 → 16 → 37 → 58 → 89
+
+	Therefore any chain that arrives at 1 or 89 will become stuck in an endless loop. What is most amazing is that EVERY starting number will eventually arrive at 1 or 89.
+
+	How many starting numbers below one million will arrive at 89?
+
+	Ans: 856929
+
+Q61. Func: 
+	Points: 120
+	Title: Expendable.
+	Content: Your work finished, the kremlin sends assassins after you, defend yourself.
+
+	It can be seen that the number, 125874, and its double, 251748, contain exactly the same digits, but in a different order.
+
+	Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits.
+
+	Ans: 142857
+
+Q66. Func: 
+	Points: 160
+	Title: Solo, A Trump Story
+	Content: No one else can be trusted, you must accomplish this on your own, infiltrate China.
+	Chinese Checkposts are arrayed in the form of Pascal's Triangle, with 51 rows, with the passable ones being those whose number in
+	the triangle is square-free, i.e., not divisible by the square of any prime.
+	Find the sum of the distinct square free numbers in these 51 rows.
+
+	Ans: 34029210557338
+
+Q68. Func: 
+	Points: 240
+	Title: Trump Power!
+	Content: You Engage a battalion in combat, Fight!!!
+	Divine authority guides your arm and you realise that the secret to smoting the enemy is held in Budget-Lychrel numbers.
+	If we take 47, reverse and add, 47 + 74 = 121, which is palindromic.
+	Not all numbers produce palindromes so quickly. For example,
+
+	349 + 943 = 1292,
+	1292 + 2921 = 4213
+	4213 + 3124 = 7337
+
+	That is, 349 took three iterations to arrive at a palindrome.
+	It has long been thought by sages that some numbers never arrive at a palindrome, since 50 is a big number, numbers that do not fall in line(become a palindrome) in 50
+	iterations are to be deemed Budget-Lychrel numbers and are the secret to victory.
+	How many Budget-Lychrel Numbers are there below ten-thousand?
+
+	Ans: 249
+
+Q71. Func: 
+	Points: 300
+	Title: The Forbidden City
+	Content:You infiltrate the forbidden city and see dead people and reptilian soldiers everywhere, fight your way into the inner sanctum.
+	To reach the inner sanctum through this carnage, you must remember the way of bounce(3D) and achieve maximum bounce!
+
+	Working from left-to-right if no digit is exceeded by the digit to its left it is called an increasing number; for example, 134468.
+	
+	Similarly if no digit is exceeded by the digit to its right it is called a decreasing number; for example, 66420.
+
+	We shall call a positive integer that is neither increasing nor decreasing a "bouncy" number; for example, 155349.
+
+	Clearly there cannot be any bouncy numbers below one-hundred, but just over half of the numbers below one-thousand (525) are bouncy. In fact, the least number for which the proportion of bouncy numbers first reaches 50% is 538.
+
+	Surprisingly, bouncy numbers become more and more common and by the time we reach 21780 the proportion of bouncy numbers is equal to 90%.
+
+	Find the least number for which the proportion of bouncy numbers is exactly 99%.
+
+	Ans: 1587000
+
+Q73. Func: 
+	Points: 800
+	Title: Duel of the Fates
+	Content: Cut off the Head and the Snake Collapses, kill Xi to save the world!
+	Defeat the other prime, and be the Hero, prime exponential!
+	The smallest number expressible as the sum of a prime square, prime cube, and prime fourth power is 28. In fact, there are exactly four numbers below fifty that can be expressed in such a way:
+
+	28 = 22 + 23 + 24
+	33 = 32 + 23 + 24
+	49 = 52 + 23 + 24
+	47 = 22 + 33 + 24
+
+	How many numbers below fifty million can be expressed as the sum of a prime square, prime cube, and prime fourth power?
+
+	Ans: 1097343
+
+Q76. Func: 
+	Points: 600
+	Title: The ***** Wars
+	Content: You will fight fire with fire, armies with armies, you will not balk, and at the end of this, YOU   WILL   BE   THE   VICTOR!
+	You've ascertained that the route to victory over the chinese is through duality, of the wes and the east, of russian and american, of cold and oil, and thus, the number 2.
+	The square root of 2 can be expressed as an infinite continued fraction. sqrt(2) =  1 + (1 / (2 + 1 / (2 + 1 / (2 + 1.....
+	Expanding this for the first three iterations:
+	1 + 1/2 = 3/2 = 1.5
+	1 + 1/(2 + 1/2) = 7/5 = 1.4
+	1 + 1/(2 + 1/(2 + 1/2) = 17/12 = 1.4166...
+	The eighth expansions is 1393/985, which is the first exxpansion where the number of digits in the numerator is mroe than that in the denominator.
+	In the first one-thousand expansions, how many fractions contain a numerator with more digits than the denominator?
+
+	Ans: 153
+
+Q78. Func: 
+	Points: 400
+	Title: Investigate
+	Content: You send your elite spies to find out the cause for your losses.
+	However, even they were spooked by what they saw, use your powers as prime to return them to a functional state.
+	Euler's Totient function, φ(n) [sometimes called the phi function], is used to determine the number of positive numbers less than or equal to n which are relatively prime to n. For example, as 1, 2, 4, 5, 7, and 8, are all less than nine and relatively prime to nine, φ(9)=6.
+	The number 1 is considered to be relatively prime to every positive number, so φ(1)=1.
+
+	Interestingly, φ(87109)=79180, and it can be seen that 87109 is a permutation of 79180.
+
+	Find the value of n, 1 < n < 107, for which φ(n) is a permutation of n and the ratio n/φ(n) produces a minimum.
+
+	Ans: 55374
+
+Q80. Func: 
+	Points: 350
+	Title: The Nuclear Option
+	Content: The reptilians have invaded this far, you must use the last option, give up your life, blow up russia and all of asia, for the future of the Earth!
+	To do this, the only prerequisite is knowledge, knowledge of this building and of nukes.
+	
+	By counting carefully it can be seen that a rectangular grid measuring 3 by 2 contains eighteen rectangles:
+
+	Although there exists no rectangular grid that contains exactly two million rectangles, find the area of the grid with the nearest solution.
+
+	Ans: 2772
 
