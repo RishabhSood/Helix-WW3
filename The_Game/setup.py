e79a4cccc55e86cec0420e5663ea018ab58db5c9
RishabhSood <55499929+RishabhSood@users.noreply.github.com>
2020-02-06 19:45:18 +0530

    Update setup.py

--- The_Game/setup.py
+++ The_Game/setup.py
@@ -1,11 +1,11 @@
 import cx_Freeze
 
-executables = [cx_Freeze.Executable("game.py")]
+executables = [cx_Freeze.Executable("game.pyw")]
 
 cx_Freeze.setup(
     name="World War III",
     options={"build_exe": {"packages":["pygame"],
-                           "include_files":["trump.png", "WW3music.mp3", "warback.jpg", "uswins.mp3", "function_lib.py", "score.txt", "ww3_log.txt"]}},
+                           "include_files":["trump.png","thefont.ttf", "cs_regular.ttf", "WW3music.mp3", "warback.jpg", "uswins.mp3", "chinatheme.mp3", "function_lib.py", "score.txt", "ww3_log.txt", "usr_details.txt"]}},
     executables = executables
 
     )
